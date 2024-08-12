a0c074aaa1db362c0bc083d3d93ecd04e669561c
mohit-aren <26.mohit@gmail.com>
2024-08-12 12:32:50 +0530

    Add files via upload

---
+++ FCN/FCN_train.py
@@ -0,0 +1,372 @@
+import numpy as np
+import pandas as pd
+
+import json
+import sys
+from PIL import Image, ImageOps
+
+#from skimage.io import imread
+#from matplotlib import pyplot as plt
+import random
+
+import os
+#os.environ['KERAS_BACKEND'] = 'theano'
+#os.environ['THEANO_FLAGS'] ='mode=FAST_RUN,device=cpu'
+#os.environ['THEANO_FLAGS'] = 'mode=FAST_RUN, device=gpu0, floatX=float32, optimizer=fast_compile'
+
+#from tensorflow.keras import models
+from keras.optimizers import SGD
+#from tensorflow.keras.layers import Input, ZeroPadding2D
+#from tensorflow.keras.layers import Activation, Flatten, Reshape
+#from tensorflow.keras.layers import Convolution2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model
+#from tensorflow.keras import utils as  np_utils
+#from keras.applications import imagenet_utils
+
+path = 'results/'
+img_w = 320
+img_h = 320
+n_labels = 2
+
+Lung = [255,255,255]
+
+Sky = [128,128,128]
+Building = [128,0,0]
+Pole = [192,192,128]
+Road_marking = [255,69,0]
+Road = [128,64,128]
+Pavement = [60,40,222]
+Tree = [128,128,0]
+SignSymbol = [192,128,128]
+Fence = [64,64,128]
+Car = [64,0,128]
+Pedestrian = [64,64,0]
+Bicyclist = [0,128,192]
+Unlabelled = [0,0,0]
+
+
+n_train = 23
+n_test = 3
+n_val = 3
+
+def label_map(labels):
+    label_map = np.zeros([img_h, img_w, n_labels])    
+    for r in range(img_h):
+        for c in range(img_w):
+            label_map[r, c, labels[r][c]] = 1
+    return label_map
+
+def label_map1(labels):
+    label_map = np.zeros([img_h, img_w, n_labels])    
+    for r in range(img_h):
+        for c in range(img_w):
+            #print(labels[r][c])
+            if(labels[r][c][0] >180 and labels[r][c][1] > 180 and labels[r][c][2] > 180):
+                label_map[r, c, 1] = 1
+            elif(labels[r][c][0] < 50 and labels[r][c][1] < 50 and labels[r][c][2] < 50):
+                label_map[r, c, 0] = 1
+    return label_map
+
+
+import os
+import imageio
+
+def prep_data1(mode):
+    data = []
+    label = []
+    
+    folder_path = 'benign' # path + mode
+
+    #images_path = os.listdir(folder_path)
+    images_path = os.listdir(folder_path)
+
+    if(mode == 'train'):
+        n = 50
+    elif(mode == 'val'):
+        n = 45
+    else:
+        n = 10
+    for index, image in enumerate(images_path):
+
+        index += 1
+        filename = os.path.join(folder_path, folder_path + ' ('+ str(index) + ').png')
+    
+        print(index, filename)
+        if(index > 50 and mode == 'train'):
+            continue
+        elif((index < 51 or index > 95) and mode =='val'):
+            continue
+        elif((index < 96 or index > 105) and mode == 'test'):
+            continue
+        
+        #truth_file = filename.split('.png')
+        truth_file = filename.split('.png')
+        
+        tfile = truth_file[0] + '_mask.png'
+    
+        
+        print(tfile)
+        if(filename == ""):
+            break
+        #img1 = Image.open(filename)
+        img1 = Image.open(filename)
+        w, h = img1.size
+        start = (w-320)//2
+        s_h = (h-320)//2
+        
+        new_im = img1.crop((start, s_h, start+320, s_h+320))
+        #new_size = tuple([544, 512])
+        
+        # create a new image and paste the resized on it
+        
+        #new_im = img1.resize((320, 320))
+        #new_im = Image.new("RGB", (544, 512))
+        #new_im.paste(img1, ((544-new_size[0])//2,
+                            #(512-new_size[1])//2))
+
+
+
+        #img2 = Image.open(tfile)
+        img2 = Image.open(tfile)
+        #new_im1 = img2.resize((320, 320))
+        new_im1 = img2.convert('RGB')
+        new_im1 = new_im1.crop((start, s_h, start+320, s_h+320))
+        #new_size = tuple([544, 512])
+        
+        # create a new image and paste the resized on it
+        
+        #new_im1 = Image.new("RGB", (544, 512))
+        #new_im1.paste(img2, ((544-new_size[0])//2,
+                            #(512-new_size[1])//2))
+
+
+        #index += 1
+        # create a new image and paste the resized on it
+        
+
+        #img, gt = [imread(path + mode + '/' + filename + '.png')], imread(path + mode + '-colormap/' + filename + '.png')
+        
+        img, gt = [np.array(new_im,dtype=np.uint8)], np.array(new_im1,dtype=np.uint8)
+        data.append(np.reshape(img,(320, 320,3)))
+        label.append(label_map1(gt))
+        sys.stdout.write('\r')
+        sys.stdout.flush()
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    data, label = np.array(data), np.array(label).reshape((n, img_h * img_w, n_labels))
+
+    print( mode + ': OK')
+    print( '\tshapes: {}, {}'.format(data.shape, label.shape))
+    print( '\ttypes:  {}, {}'.format(data.dtype, label.dtype))
+    #print( '\tmemory: {}, {} MB'.format(data.nbytes / 1048544, label.nbytes / 1048544))
+
+    return data, label
+    
+
+
+
+def prep_data(mode):
+    assert mode in {'test', 'train'}, \
+        'mode should be either \'test\' or \'train\''
+    data = []
+    label = []
+    df = pd.read_csv(path + mode + '.csv')
+    n = n_train if mode == 'train' else n_test
+    for i, item in df.iterrows():
+        if i >= n:
+            break
+        img, gt = [imread(path + item[0])], np.clip(imread(path + item[1]), 0, 1)
+        data.append(np.reshape(img,(256,256,1)))
+        label.append(label_map(gt))
+        sys.stdout.write('\r')
+        sys.stdout.write(mode + ": [%-20s] %d%%" % ('=' * int(20. * (i + 1) / n - 1) + '>',
+                                                    int(100. * (i + 1) / n)))
+        sys.stdout.flush()
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    data, label = np.array(data), np.array(label).reshape((n, img_h * img_w, n_labels))
+
+    print( mode + ': OK')
+    print( '\tshapes: {}, {}'.format(data.shape, label.shape))
+    print( '\ttypes:  {}, {}'.format(data.dtype, label.dtype))
+    print( '\tmemory: {}, {} MB'.format(data.nbytes / 1048576, label.nbytes / 1048576))
+
+    return data, label
+
+"""
+def plot_results(output):
+    gt = []
+    df = pd.read_csv(path + 'test.csv')
+    for i, item in df.iterrows():
+        gt.append(np.clip(imread(path + item[1]), 0, 1))
+
+    plt.figure(figsize=(15, 2 * n_test))
+    for i, item in df.iterrows():
+        plt.subplot(n_test, 4, 4 * i + 1)
+        plt.title('Ground Truth')
+        plt.axis('off')
+        gt = imread(path + item[1])
+        plt.imshow(np.clip(gt, 0, 1))
+
+        plt.subplot(n_test, 4, 4 * i + 2)
+        plt.title('Prediction')
+        plt.axis('off')
+        labeled = np.argmax(output[i], axis=-1)
+        plt.imshow(labeled)
+
+        plt.subplot(n_test, 4, 4 * i + 3)
+        plt.title('Heat map')
+        plt.axis('off')
+        plt.imshow(output[i][:, :, 1])
+
+        plt.subplot(n_test, 4, 4 * i + 4)
+        plt.title('Comparison')
+        plt.axis('off')
+        rgb = np.empty((img_h, img_w, 3))
+        rgb[:, :, 0] = labeled
+        rgb[:, :, 1] = imread(path + item[0])
+        rgb[:, :, 2] = gt
+        plt.imshow(rgb)
+
+    plt.savefig('result.png')
+    plt.show()
+"""
+
+#########################################################################################################
+
+#from tensorflow.keras.layers import Input
+#from tensorflow.keras.layers import Convolution2D
+#from tensorflow.keras.layers import Activation, Reshape
+#from tensorflow.keras.layers import BatchNormalization
+#from tensorflow.keras.models import Model
+#from layers import MaxPoolingWithArgmax2D, MaxUnpooling2D
+
+
+import numpy as np
+#import matplotlib.pyplot as plt
+#from pylab import *
+import os
+import sys
+#from tensorflow.keras_contrib.applications import densenet
+#from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.layers import *
+#from tensorflow.keras.engine import Layer
+#from tensorflow.keras.applications.vgg16 import *
+from tensorflow.keras.models import *
+#from keras_applications.imagenet_utils import _obtain_input_shape
+#import keras.backend as K
+import tensorflow as tf
+
+from utils.get_weights_path import *
+from utils.basics import *
+from utils.resnet_helpers import *
+from utils.BilinearUpSampling import *
+
+def FCN_Vgg16_32s(input_shape=(320, 320, 3), weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=2):
+    if batch_shape:
+        img_input = Input(batch_shape=batch_shape)
+        image_size = batch_shape[1:3]
+    else:
+        img_input = Input(shape=input_shape)
+        image_size = input_shape[0:2]
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', kernel_regularizer=l2(weight_decay))(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Convolutional layers transfered from fully-connected layers
+    x = Conv2D(4096, (7, 7), activation='relu', padding='same', name='fc1', kernel_regularizer=l2(weight_decay))(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(4096, (1, 1), activation='relu', padding='same', name='fc2', kernel_regularizer=l2(weight_decay))(x)
+    x = Dropout(0.5)(x)
+    #classifying layer
+    x = Conv2D(classes, (1, 1), kernel_initializer='he_normal', activation='linear', padding='valid', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+
+    x = BilinearUpSampling2D(size=(32, 32))(x)
+    x = Reshape((320*320, 2))(x)
+    x = Activation("softmax")(x)
+
+    model = Model(img_input, x)
+
+    #weights_path = os.path.expanduser(os.path.join('~', '.keras/models/fcn_vgg16_weights_tf_dim_ordering_tf_kernels.h5'))
+    #model.load_weights(weights_path, by_name=True)
+    return model
+
+
+autoencoder = FCN_Vgg16_32s()
+
+print('Start')
+optimizer = SGD(lr=0.001, momentum=0.9, decay=0.0005, nesterov=False)
+autoencoder.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+print( 'Compiled: OK')
+autoencoder.summary()
+
+# Train model or load weights
+
+train_data, train_label = prep_data1('train')
+val_data, val_label = prep_data1('val')
+nb_epoch = 100
+batch_size = 2
+history = autoencoder.fit(train_data, train_label, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(val_data, val_label))
+autoencoder.save_weights('model_5l_weight_fcn_lungs.1.hdf5')
+
+#autoencoder.load_weights('model_5l_weight_ep50.hdf5')
+
+
+layer1_filters = 64
+layer2_filters = 64
+layer3_filters = 128
+layer4_filters = 128
+layer5_filters = 256
+layer6_filters = 256
+layer7_filters = 256
+layer8_filters = 512
+layer9_filters = 512
+layer10_filters = 512
+layer11_filters = 512
+layer12_filters = 512
+layer13_filters = 512
+layer14_filters = 512
+layer15_filters = 512
+layer16_filters = 512
+layer17_filters = 512
+layer18_filters = 512
+layer19_filters = 256
+layer20_filters = 256
+layer21_filters = 256
+layer22_filters = 128
+layer23_filters = 128
+layer24_filters = 64
+layer25_filters = 64
+
+test_data, test_label = prep_data1('test')
+
+score = autoencoder.evaluate(test_data, test_label, verbose=1)
+print( 'Test score:', score[0])
+print( 'Test accuracy:', score[1])
+
